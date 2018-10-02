973e4d0ccd1ad8042648eb2e296c5612eccfdabf
alexhills44 <alexhills444@gmail.com>
2018-10-02 15:54:41 +0300

    Added PropertiesXpath Class

---
+++ src/main/java/PropertiesXpath.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesXpath {
+    private static Properties prop;
+    private static FileInputStream input;
+
+    PropertiesXpath () {
+        prop = new Properties();
+    }
+
+    public static String getProp(String key) {
+        try {
+            input = new FileInputStream(new File("config.properties"));
+            prop.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return prop.getProperty(key);
+    }
+}
